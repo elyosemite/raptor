f6081f4a4903e9ab09f750f6d6c0301641b63d39
Yuri Melo <yurifullstack@gmail.com>
2026-06-18 22:06:35 -0300

    Fix config file path in load function to use raptor.toml

--- src/config.rs
+++ src/config.rs
@@ -6,7 +6,7 @@ use std::fs;
 
 pub fn load() -> anyhow::Result<Config> {
     let content =
-        fs::read_to_string("config.toml")?;
+        fs::read_to_string("raptor.toml")?;
 
     let config =
         toml::from_str(&content)?;
